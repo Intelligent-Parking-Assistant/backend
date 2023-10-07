a7e37daad7684fb71a841a390bdcdec3fc8c6fc1
allankariuki <alankariuki24@gmail.com>
2023-10-07 21:05:14 +0300

    Find parking functionality and directions listing

--- ipa/views.py
+++ ipa/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .serializers import ParkingSerializer, VisitsSerializer
 
+#Admin CRUD
 class ParkingViewset(viewsets.ViewSet):
     def list(self, request):
         queryset = Parkings.objects.all()
@@ -20,7 +21,8 @@ class ParkingViewset(viewsets.ViewSet):
             {'details': serializer.data, 'code': 200},
             status= status.HTTP_200_OK
         )   
-        
+    
+    #Creation of parking, done by admin 
     def create(self, request):
         serializer = ParkingSerializer(data=request.data)
         
@@ -83,8 +85,57 @@ class ParkingViewset(viewsets.ViewSet):
         )
         
 
+class FindParkingViewset(viewsets.ViewSet):
+    #listing of parkings to the user based on the 1Km radius.
+    #Here we'll incorporate the algorithm to find distance and calculate it for us.
+    #This will also serve as the directions page
+    def list(self, request):
+        queryset = Parkings.objects.all()
+        
+        serializer = ParkingSerializer(queryset, many = True)
+        if not serializer.data:
+            return Response(
+                {'details': 'No parkings yet', 'code': 200},
+                status= status.HTTP_200_OK
+            )
+         
+        return Response(
+            {'details': serializer.data, 'code': 200},
+            status= status.HTTP_200_OK
+        )   
+    
+    def create(self, request):
+        pass
+        
+    #single view of a parking
+    #We'll incorporate the google maps API
+    # and passing the image to the ML model
+    def retrieve(self, request, pk = None):
+        try:
+            parking = Parkings.objects.get(id = pk)
+        except Parkings.DoesNotExist:
+            return Response(
+                {'details': 'Parking does nto exist', 'code': 400},
+                status= status.HTTP_400_BAD_REQUEST
+            )
+            
+        serializer = ParkingSerializer(parking)
+        return Response(
+                {'details': serializer.data, 'code': 200},
+                status= status.HTTP_200_OK
+            )
+        
+    def update(self, request, pk = None):
+        pass
+        
+    def destroy(self, request, pk = None):
+        pass
+        
+
 #Visists Viewset
+
 class VisitsViewset(viewsets.ViewSet):
+    #Listing of users history 
     def list(self, request):
         queryset = Visits.objects.all()
         
@@ -100,6 +151,7 @@ class VisitsViewset(viewsets.ViewSet):
             status= status.HTTP_200_OK
         )   
         
+    #Registration of user's history or complete visit
     def create(self, request):
         user = request.data.get('user')
         parking = request.data.get('parking')
@@ -129,7 +181,8 @@ class VisitsViewset(viewsets.ViewSet):
                 {'details': 'Visit added successfully', 'code': 200},
                 status= status.HTTP_200_OK
             )
-        
+      
+    #single retrieval of user's hostory and possible direction check 
     def retrieve(self, request, pk = None):
         try:
             visit = Visits.objects.get(id = pk)
