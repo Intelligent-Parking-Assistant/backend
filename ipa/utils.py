44ea6681cc24ce0a933ce8c3a06267a5c3db4c41
allankariuki <alankariuki24@gmail.com>
2023-10-08 08:41:18 +0300

    Display parkings list

--- ipa/utils.py
+++ ipa/utils.py
@@ -3,12 +3,25 @@ from haversine import haversine
 from .models import Parkings
 
 parkings = Parkings.objects.all()
+locations = []
+latitude = []
+longitude = []
+
+for parking in parkings:
+    locations.append(parking.location)
+    latitude.append(parking.latitude)
+    longitude.append(parking.longitude)
+    
 
 def getParkings():
     df = pd.DataFrame({
-        'location': ['Norean', 'OKC', 'New York', 'Bayonne'],
-        'latitude': [35.221, 35.463, 41.112, 40.66],
-        'longitude': [-97.481, -97.622, -74.1083, -74.106]
+        # 'location': '''['Norean', 'OKC', 'New York', 'Bayonne']''',
+        # 'latitude':  '''[35.221, 35.463, 41.112, 40.66]''',
+        # 'longitude':  '''[-97.481, -97.622, -74.1083, -74.106]'''
+        
+        'location': locations,
+        'latitude': latitude,
+        'longitude': longitude
     })
 
     jersey_city_lat_lon = (40.94, -74.05)
