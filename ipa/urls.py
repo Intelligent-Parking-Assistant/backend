ef1651c0a23edd490d043960151a96f468921fc1
allankariuki <alankariuki24@gmail.com>
2023-10-20 23:54:25 +0300

    Switching to function based views and implementing fetch of images using google maps

--- ipa/urls.py
+++ ipa/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ParkingViewset, VisitsViewset, FindParkingViewset
-
-router = DefaultRouter()
-
-router.register(r'admin-parkings', ParkingViewset, basename='parkings')
-router.register(r'visits', VisitsViewset, basename='visits')
-router.register(r'parkings', FindParkingViewset, basename='find-parking')
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('directions/', views.directions, name= 'directions'),
+    path('search/', views.search_parking, name= 'search'),
+    path('visits/<id>/', views.visits, name= 'visits')
 ]
